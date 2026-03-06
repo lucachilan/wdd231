665f38c2ebbc8dfae5863274193415c36edd8e3f
Luca Chilan <lucachilan@gmail.com>
2026-03-06 03:55:53 -0300

    index update, all content except certificate courses

---
+++ scripts/date.js
@@ -0,0 +1,6 @@
+const year = new Date().getFullYear();
+const lastModified = document.lastModified;
+
+document.getElementById("currentyear").textContent = year;
+
+document.getElementById("lastModified").textContent = "Last modification: " + lastModified;
