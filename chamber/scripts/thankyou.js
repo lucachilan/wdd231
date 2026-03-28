d78da1812d3bcf19449ce6137b95f6ce10291764
Luca Chilan <lucachilan@gmail.com>
2026-03-27 23:40:06 -0300

    fixing html errors with labels inside labels

--- chamber/scripts/thankyou.js
+++ chamber/scripts/thankyou.js
@@ -30,10 +30,17 @@ const year = dateObj.getFullYear();
 
 const myDate = `${month} ${day}${suffix}, ${year}`;
 
+let membership;
+if (myINFO.get('membership') === 'np') {
+    membership = 'Non Profit';
+} else {
+    membership = myINFO.get('membership');
+}
+
 // first name, last name, email, mobile number, business name, and current date timestamp from the hidden field
 document.querySelector('#results').innerHTML = `
 <h2 class="name">${myINFO.get('first')} ${myINFO.get('last')},</h2>
-<p>${myINFO.get('org-name')} is now a ${myINFO.get('membership')} member of the Rivera Chamber of Commerce</p>
+<p>${myINFO.get('org-name')} is now a ${membership} member of the Rivera Chamber of Commerce</p>
 <p>In case we need it, we will contact you at: </p>
 <div class="contact-info">
     <p>Your email: ${myINFO.get('email')}</p>
